6b0734d96088142d589190c2ad8f0bd11e6c951d
Username <bill.cann27@gmail.com>
2022-02-22 10:31:19 +0000

    stretch goal completed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,13 @@ const app = (yargsObj) => {
         const movie = new Movie(yargsObj.title, yargsObj.actor)
         movie.add();
         console.log(movie.list());
-        //add movie to array, console.log at end
+    } else if (yargsObj.addMulti) {
+        const movie1 = new Movie(yargsObj.title1, yargsObj.actor1);
+        const movie2 = new Movie(yargsObj.title2, yargsObj.actor2);
+        movie1.add();
+        movie2.add();
+        console.log(movie1.list());
+        console.log(movie2.list());
     } else {
         console.log("incorrect command");
     }
